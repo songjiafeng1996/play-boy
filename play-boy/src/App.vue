12761502f6426cd97d97004dd8fb5e614d83deff
e <2055637690@qq.com>
2018-10-12 19:16:14 +0800

    十月十二

---
+++ play-boy/src/App.vue
@@ -0,0 +1,156 @@
+<template>
+  <div id="app">
+    <div class="nav-top">
+      <div class="nav-top-left">
+        <router-link to="">
+          <div class="movie-first-stop fl">
+            <img src="./assets/images/logo.png" alt="">
+          </div>
+        </router-link>
+        <router-link to="">
+          <div class="movie top-com fl">
+            <span>电影</span>
+          </div>
+        </router-link>
+        <router-link to="">
+          <div class="TV-play top-com fl">
+            <span>电视剧</span>
+          </div>
+        </router-link>
+        <router-link to="">
+          <div class="Comic top-com fl">
+            <span>动漫</span>
+          </div>
+        </router-link>
+        <router-link to="">
+          <div class="nineth-area top-com fl">
+            <span>第九区</span>
+          </div>
+        </router-link>
+        <router-link to="">
+          <div class="forest top-com fl">
+            <span>小森林</span>
+          </div>
+        </router-link>
+        <router-link to="">
+          <div class="more top-com fl">
+            <span>更多</span>
+          </div>
+        </router-link>
+      </div>
+      <div class="nav-top-right">
+        <router-link to="">
+          <span class="register fr">注册</span>
+        </router-link>
+        <router-link to="">
+          <span class="login fr">登录</span>
+        </router-link>
+        <div class="search-box fr">
+          <input type="text" class="search">
+          <div class="btn-search">搜索</div>
+        </div>
+      </div>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "app",
+};
+</script>
+
+<style lang="scss">
+a{
+  color:#fff;
+}
+html,body{
+  background-color: #840308;
+}
+#app {
+  width: 100%;
+
+  .nav-top {
+    height: 42px;
+    width: 100%;
+    background-color: #ff6666;
+    font-size: 14px;
+    color: #fff;
+    .nav-top-left {
+      width:650px;
+      float: left;
+      .movie-first-stop {
+        height: 42px;
+        width: 120px;
+        margin-left: 2%;
+      }
+      .top-com {
+        width: 73px;
+        height: 42px;
+        background: url("./assets/images/i3.png") 10px 10px no-repeat;
+        line-height: 42px;
+        span {
+          padding-left: 35px;
+        }
+      }
+      .TV-play {
+        width: 88px;
+        background: url("./assets/images/i3.png") 10px -30px no-repeat;
+      }
+      .Comic {
+        background: url("./assets/images/i3.png") 10px -70px no-repeat;
+      }
+      .nineth-area {
+        width:90px;
+        background: url("./assets/images/i3.png") 10px -310px no-repeat;
+      }
+      .forest {
+        width: 88px;
+        background: url("./assets/images/i3.png") 10px -190px no-repeat;
+      }
+      .more {
+        width:100px;
+        background: url("./assets/images/i3.png") 10px -148px no-repeat;
+      }
+    }
+    .nav-top-right {
+      float: right;
+      height: 42px;
+      margin-right:30px;
+      // line-height: 42px;
+      .register {
+         padding:0 10px;
+         height: 18px;
+         margin-top:12px;
+         border-left:1px solid #c1c1c1;
+      }
+      .login{
+        margin-top:12px;
+         padding:0 10px;
+      }
+      .search-box{
+        width:192px;
+        height: 30px;
+        border:1px solid #6cb5f4;
+        margin-top:5px;
+        .search{
+         width:142px;
+         height: 28px;
+         float: left;
+        }
+        .btn-search{
+          width:46px;
+          height: 30px;
+          background-color: #6cb5f4;
+          color:#fff;
+          line-height: 28px;
+          text-align:center;
+          float: left;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ play-boy/src/components/Home.vue
@@ -0,0 +1,578 @@
+<template>
+  <div class="home-content">
+    <div class="center-search-content">
+      <div class="search">
+        <input type="text" placeholder="请输入搜索内容">
+      </div>
+      <div class="btn-search">
+        搜索
+      </div>
+    </div>
+    <div class="main-content ">
+      <div class="main-content-left clearfloat fl">
+        <div class="gap"></div>
+        <div class="left-list-items clearfloat">
+          <div class="top">
+            <div class="top-title">
+              过境 Transit 【蓝光1080p内嵌中文字幕】【2018】【剧情】【德国/法国】
+            </div>
+            <div class="top-score">
+              <span class="name">豆瓣：</span><span class="code">7.2</span>
+            </div>
+          </div>
+          <div class="bottom-content">
+            <div class="left-img">
+              <img src="../assets/images/beauty.jpg" alt="">
+            </div>
+            <div class="right-description">
+              <span class="description-info">德国军队就在巴黎城外。格奥尔在最后一刻逃到了马赛。他的行李中带着一个叫Weidel的作家的遗产，这个作害怕遭受迫害而选择了自尽。这些遗产中有一份手稿，一些信件，和墨西哥大使馆对签证的一份担保。只有这些才能证明他们已被允许从这座港口城市离开，离开这里意味着还需要一份来自潜在东道国的入境许可。以Weidel的假身份，格奥尔试图获取船上的一些稀缺通道。难民之间的会谈在他小旅馆的走廊上，领事馆的等候室里，以及港口边上的咖啡厅和酒吧中进行着。格奥尔帮忙照顾着他已逝战友海因茨的儿子，海因茨在逃亡时不幸过世。但他的计划在遇见那个神秘姑娘玛丽时改变了。《过境》基于德国女作家安娜·西格斯撰写的二战同名小说改编，电影以现代马赛为背景，来自过去的人们在这里四处出现。因此，过去的难民会与今日的难民相遇，历史与今生相遇，他们所有的故事融合在一起构成一个永恒的过境空间。</span>
+              <router-link to=""><span class="thunderbolt-download">迅雷电影下载</span></router-link>
+              <div class="operation-ways">
+                <span class="com">发布</span> <span class="time">2018-10-12</span><span class="read">阅读</span><span>|</span>
+                <span class="com">标签：</span><span class="move-about">剧情</span><span class="move-about">高清电影下载</span><span>|</span>
+                <span class="com">info</span><span class="move-about">豆瓣</span><span class="move-about">imdb</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="left-list-items clearfloat">
+          <div class="top">
+            <div class="top-title">
+              过境 Transit 【蓝光1080p内嵌中文字幕】【2018】【剧情】【德国/法国】
+            </div>
+            <div class="top-score">
+              <span class="name">豆瓣：</span><span class="code">7.2</span>
+            </div>
+          </div>
+          <div class="bottom-content">
+            <div class="left-img">
+              <img src="../assets/images/beauty.jpg" alt="">
+            </div>
+            <div class="right-description">
+              <span class="description-info">德国军队就在巴黎城外。格奥尔在最后一刻逃到了马赛。他的行李中带着一个叫Weidel的作家的遗产，这个作害怕遭受迫害而选择了自尽。这些遗产中有一份手稿，一些信件，和墨西哥大使馆对签证的一份担保。只有这些才能证明他们已被允许从这座港口城市离开，离开这里意味着还需要一份来自潜在东道国的入境许可。以Weidel的假身份，格奥尔试图获取船上的一些稀缺通道。难民之间的会谈在他小旅馆的走廊上，领事馆的等候室里，以及港口边上的咖啡厅和酒吧中进行着。格奥尔帮忙照顾着他已逝战友海因茨的儿子，海因茨在逃亡时不幸过世。但他的计划在遇见那个神秘姑娘玛丽时改变了。《过境》基于德国女作家安娜·西格斯撰写的二战同名小说改编，电影以现代马赛为背景，来自过去的人们在这里四处出现。因此，过去的难民会与今日的难民相遇，历史与今生相遇，他们所有的故事融合在一起构成一个永恒的过境空间。</span>
+              <router-link to=""><span class="thunderbolt-download">迅雷电影下载</span></router-link>
+              <div class="operation-ways">
+                <span class="com">发布</span> <span class="time">2018-10-12</span><span class="read">阅读</span><span>|</span>
+                <span class="com">标签：</span><span class="move-about">剧情</span><span class="move-about">高清电影下载</span><span>|</span>
+                <span class="com">info</span><span class="move-about">豆瓣</span><span class="move-about">imdb</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="left-list-items clearfloat">
+          <div class="top">
+            <div class="top-title">
+              过境 Transit 【蓝光1080p内嵌中文字幕】【2018】【剧情】【德国/法国】
+            </div>
+            <div class="top-score">
+              <span class="name">豆瓣：</span><span class="code">7.2</span>
+            </div>
+          </div>
+          <div class="bottom-content">
+            <div class="left-img">
+              <img src="../assets/images/beauty.jpg" alt="">
+            </div>
+            <div class="right-description">
+              <span class="description-info">德国军队就在巴黎城外。格奥尔在最后一刻逃到了马赛。他的行李中带着一个叫Weidel的作家的遗产，这个作害怕遭受迫害而选择了自尽。这些遗产中有一份手稿，一些信件，和墨西哥大使馆对签证的一份担保。只有这些才能证明他们已被允许从这座港口城市离开，离开这里意味着还需要一份来自潜在东道国的入境许可。以Weidel的假身份，格奥尔试图获取船上的一些稀缺通道。难民之间的会谈在他小旅馆的走廊上，领事馆的等候室里，以及港口边上的咖啡厅和酒吧中进行着。格奥尔帮忙照顾着他已逝战友海因茨的儿子，海因茨在逃亡时不幸过世。但他的计划在遇见那个神秘姑娘玛丽时改变了。《过境》基于德国女作家安娜·西格斯撰写的二战同名小说改编，电影以现代马赛为背景，来自过去的人们在这里四处出现。因此，过去的难民会与今日的难民相遇，历史与今生相遇，他们所有的故事融合在一起构成一个永恒的过境空间。</span>
+              <router-link to=""><span class="thunderbolt-download">迅雷电影下载</span></router-link>
+              <div class="operation-ways">
+                <span class="com">发布</span> <span class="time">2018-10-12</span><span class="read">阅读</span><span>|</span>
+                <span class="com">标签：</span><span class="move-about">剧情</span><span class="move-about">高清电影下载</span><span>|</span>
+                <span class="com">info</span><span class="move-about">豆瓣</span><span class="move-about">imdb</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="left-list-items clearfloat">
+          <div class="top">
+            <div class="top-title">
+              过境 Transit 【蓝光1080p内嵌中文字幕】【2018】【剧情】【德国/法国】
+            </div>
+            <div class="top-score">
+              <span class="name">豆瓣：</span><span class="code">7.2</span>
+            </div>
+          </div>
+          <div class="bottom-content">
+            <div class="left-img">
+              <img src="../assets/images/beauty.jpg" alt="">
+            </div>
+            <div class="right-description">
+              <span class="description-info">德国军队就在巴黎城外。格奥尔在最后一刻逃到了马赛。他的行李中带着一个叫Weidel的作家的遗产，这个作害怕遭受迫害而选择了自尽。这些遗产中有一份手稿，一些信件，和墨西哥大使馆对签证的一份担保。只有这些才能证明他们已被允许从这座港口城市离开，离开这里意味着还需要一份来自潜在东道国的入境许可。以Weidel的假身份，格奥尔试图获取船上的一些稀缺通道。难民之间的会谈在他小旅馆的走廊上，领事馆的等候室里，以及港口边上的咖啡厅和酒吧中进行着。格奥尔帮忙照顾着他已逝战友海因茨的儿子，海因茨在逃亡时不幸过世。但他的计划在遇见那个神秘姑娘玛丽时改变了。《过境》基于德国女作家安娜·西格斯撰写的二战同名小说改编，电影以现代马赛为背景，来自过去的人们在这里四处出现。因此，过去的难民会与今日的难民相遇，历史与今生相遇，他们所有的故事融合在一起构成一个永恒的过境空间。</span>
+              <router-link to=""><span class="thunderbolt-download">迅雷电影下载</span></router-link>
+              <div class="operation-ways">
+                <span class="com">发布</span> <span class="time">2018-10-12</span><span class="read">阅读</span><span>|</span>
+                <span class="com">标签：</span><span class="move-about">剧情</span><span class="move-about">高清电影下载</span><span>|</span>
+                <span class="com">info</span><span class="move-about">豆瓣</span><span class="move-about">imdb</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="bottom-page " >
+          <Page :total="100" prev-text="上一页" next-text="下一页" show-elevator />
+        </div>
+        <div class="needGAP">
+          <div class="bottom-explain">
+      <p class="explain">本站所有视频链接均从互联网收集而来，版权归原创者所有，如果侵犯了你的权益，请通知我们( 2172657323@qq.com )，我们会及时删除侵权内容，谢谢合作！
+       </p>
+       <p class="setTime">© 2018 dysfz.vip</p>
+    </div>
+        </div>
+      </div>
+      <div class="main-content-right fr">
+        <div class="product clearfloat">
+          <img src="../assets/images/product.jpg" alt="">
+          <div class="product-info">
+            <div class="description">秋冬男童2两件套</div>
+            <div class="price clearfloat">
+              <div class="left">￥<span class="money">39.9</span>券后价</div>
+              <div class="right">
+                <img src="../assets/images/icon-q.png" alt="">
+                <div class="discount-price">20元</div>
+              </div>
+            </div>
+            <div class="out-favorite">
+              <div class="favorite fr"><img src="../assets/images/icon_favorite_78d7777.png" alt="">
+                <div class="fl">2172</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="movie-category clearfloat">
+          <div class="top-title">电影分类</div>
+          <div class="content-category ">
+            <router-link to="">
+              <div class="category-item">
+                <div class="add"><img src="../assets/images/add (3).png" alt=""></div>
+                <div class="name">动画</div>
+              </div>
+            </router-link>
+            <div class="category-item">
+              <div class="add"><img src="../assets/images/add (3).png" alt=""></div>
+              <div class="name">动画</div>
+            </div>
+            <div class="category-item">
+              <div class="add"><img src="../assets/images/add (3).png" alt=""></div>
+              <div class="name">动画</div>
+            </div>
+            <div class="gap" style="height:15px;"></div>
+          </div>
+        </div>
+        <div class="tabs-all">
+          <div class="top-title">电影分类</div>
+          <div class="tabs-content clearfloat">
+            <li>动画</li>
+            <li>奇幻</li>
+            <li>冒险</li>
+            <li>韩剧</li>
+            <li>台剧</li>
+            <li>动画</li>
+            <li>奇幻</li>
+            <li>冒险</li>
+            <li>韩剧</li>
+            <li>台剧</li>
+            <li>动画</li>
+            <li>奇幻</li>
+            <li>冒险</li>
+            <li>韩剧</li>
+            <li>台剧</li>
+            <li>动画</li>
+            <li>奇幻</li>
+            <li>冒险</li>
+            <li>韩剧</li>
+            <li>台剧</li>
+          </div>
+        </div>
+        <div class="save-money">
+          <img src="../assets/images/savemoney.jpg" alt="">
+        </div>
+        <div class="friendship-link">
+          <div class="top-title">电影分类</div>
+          <div class="content">
+            <router-link to="">
+              <div class="list-item">小森林导航</div>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- <div class="bottom-explain">
+      <p class="explain">本站所有视频链接均从互联网收集而来，版权归原创者所有，如果侵犯了你的权益，请通知我们( 2172657323@qq.com )，我们会及时删除侵权内容，谢谢合作！
+       </p>
+       <p class="setTime">© 2018 dysfz.vip</p>
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.needGAP{
+  height: 200px;
+  position: relative;
+  .bottom-explain{
+    position: relative;
+    z-index:6666;
+  }
+}
+.home-content {
+  width: 960px;
+  margin: 0 auto;
+  height: auto;
+  border-radius:16px!important;
+  .center-search-content {
+    width: 600px;
+    height: 52px;
+    margin: 30px auto;
+    .search {
+      width: 500px;
+      height: 52px;
+      float: left;
+      overflow: hidden;
+      input {
+        width: 100%;
+        height: 100%;
+        font-size: 20px;
+        padding-left: 20px;
+      }
+    }
+    .btn-search {
+      float: left;
+      width: 96px;
+      height: 52px;
+      background-color: #fbcc06;
+      line-height: 52px;
+      text-align: center;
+      font-size: 24px;
+    }
+  }
+  .main-content {
+    width: 960px;
+    height: auto;
+    background-color: #fff;
+    margin-bottom:100px;
+    .main-content-left {
+      width: 690px;
+      height: auto;
+      border-radius: 10px;
+      overflow: hidden;
+      // background-color: #fff;
+      // margin-right:10px;
+      .gap {
+        height: 20px;
+         background-color: #fff;
+      }
+
+      .left-list-items {
+        padding: 10px 20px;
+        border-bottom: 1px solid #ccc;
+         background-color: #fff;
+        .top {
+          .top-title {
+            width: 580px;
+            float: left;
+            height: 70px;
+            font-size: 19px;
+            font-weight: 600;
+          }
+          .top-score {
+            width: 54px;
+            float: right;
+            font-size: 12px;
+            .name {
+              color: #ccc;
+            }
+            .code {
+              color: red;
+            }
+          }
+        }
+        .bottom-content {
+           background-color: #fff;
+          margin-top: 20px;
+          .left-img {
+            width: 130px;
+            height: 130px;
+            float: left;
+            img {
+              width: 100px;
+              height: 130px;
+              margin: 0 15px;
+            }
+          }
+          .right-description {
+            float: left;
+            width: 500px;
+            height: auto;
+            .description-info {
+              font-size: 14px;
+            }
+            .thunderbolt-download {
+              font-size: 14px;
+              color: #999;
+            }
+            .operation-ways {
+              height: 60px;
+              line-height: 60px;
+              span {
+                font-size: 12px;
+                padding: 0 2px;
+              }
+              .com {
+                color: #999;
+              }
+              .time {
+                color: #4fc72a;
+              }
+              .read {
+                color: #666;
+              }
+              .move-about {
+                color: #12c;
+              }
+            }
+          }
+        }
+      }
+      .bottom-page {
+        width: 100%;
+        height: 40px;
+        background-color: #fff;
+      }
+      /deep/ .ivu-page {
+        float: right;
+      }
+    }
+    .main-content-right {
+      width: 260px;
+      // background-color: red!important;
+      .product {
+        width: 260px;
+        background-color: #fff;
+        border-radius: 10px;
+        overflow: hidden;
+        border:1px solid red;
+        img {
+          width: 260px;
+          height: 260px;
+        }
+        .product-info {
+          .description {
+            text-align: left;
+            margin-left: 10px;
+            font-size: 14px;
+            color: #999;
+            height: 30px;
+            line-height: 30px;
+            margin-right:5px;
+          }
+          .price {
+            margin: 0 10px;
+            .left {
+              height: 30px;
+              float: left;
+              line-height: 30px;
+              font-size: 14px;
+              span {
+                font-weight: 700;
+                color: red;
+              }
+            }
+            .right {
+              width: 60px;
+              height: 16px;
+              margin-top: 7px;
+              float: right;
+              position: relative;
+              img {
+                position: absolute;
+                left: 0;
+                top: 0;
+                width: 16px;
+                height: 16px;
+                z-index: 11;
+              }
+              .discount-price {
+                position: absolute;
+                right: 0;
+                top: 0;
+                width: 45px;
+                height: 16px;
+                border: 1px solid red;
+                color: red;
+                line-height: 16px;
+                text-align: center;
+              }
+            }
+          }
+          .out-favorite {
+            height: 50px;
+            margin: 0 10px;
+
+            .favorite {
+              width: 50px;
+              height: 16px;
+              margin-top: 17px;
+              img {
+                width: 16px;
+                height: 16px;
+                float: left;
+                margin-right: 5px;
+              }
+            }
+          }
+        }
+      }
+      .movie-category {
+        width: 260px;
+        height: auto;
+        background-color: #fff;
+        border-radius: 10px;
+         border:1px solid red;
+         margin-top:10px;
+        .top-title {
+          width: 240px;
+          font-size: 14px;
+          font-weight: 600;
+          margin: 10px 10px 0;
+          text-align: left;
+          border-bottom: 1px solid #ccc;
+        }
+        .content-category {
+          margin: 10px;
+          .category-item {
+            width: 240px;
+            background-color: #ccc;
+            margin-bottom: 5px;
+            height: 30px;
+            display: flex;
+            align-items: center;
+            border-radius: 3px;
+            .add {
+              width: 16px;
+              height: 16px;
+              float: left;
+              margin-left: 10px;
+              img {
+                width: 100%;
+                height: 100%;
+              }
+            }
+            .name {
+              font-size: 16px;
+              float: left;
+              margin-left: 20px;
+              color: #333;
+            }
+          }
+        }
+      }
+      .tabs-all {
+        width: 260px;
+        height: auto;
+        background-color: #fff;
+        border-radius: 10px;
+       margin-top:10px;
+         border:1px solid red;
+        .top-title {
+          width: 240px;
+          font-size: 14px;
+          font-weight: 600;
+          margin: 10px 10px 0;
+          text-align: left;
+          border-bottom: 1px solid #ccc;
+        }
+        .tabs-content {
+          margin: 10px;
+          background-color: #fff;
+          li {
+            font-size: 12px;
+            width: 36px;
+            height: 30px;
+            border-radius: 3px;
+            text-align: center;
+            line-height: 30px;
+            background-color: #ebe8e9;
+            float: left;
+            list-style: none;
+            margin: 3px;
+          }
+          li:hover {
+            background-color: #878485;
+          }
+        }
+      }
+      .save-money {
+         border:1px solid red;
+        width: 260px;
+        height: 164px;
+        border: 5px solid #fff;
+        background-color: #fe0036;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 5px;
+        margin-top:10px;
+        img {
+          width: 250px;
+          height: 112px;
+        }
+      }
+      .friendship-link {
+        background-color: #fff;
+        width: 260px;
+        border-radius: 10px;
+         border:1px solid red;
+        height: auto;
+        margin-top:10px;
+        .top-title {
+          width: 240px;
+          font-size: 14px;
+          font-weight: 600;
+          margin: 10px 10px 0;
+          text-align: left;
+
+          border-bottom: 1px solid #ccc;
+        }
+        .content {
+          margin: 10px;
+          .list-item {
+            color:#333;
+            height: 30px;
+            line-height: 30px;
+            font-size: 14px;
+          }
+        }
+      }
+    }
+  }
+  .bottom-explain{
+    height: 100px;
+    width:690px;
+    margin:30px auto;
+    font-size:14px;
+    color:#fff;
+    .explain{
+       width:690px;
+      text-align:center;
+      height: 30px;
+      line-height: 30px;
+    }
+    .setTime{
+        text-align:center;
+      height: 30px;
+      line-height: 30px;
+      margin-top:30px;
+    }
+
+  }
+  
+}
+</style>
